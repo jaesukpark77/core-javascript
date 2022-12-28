c375ac4da79f424ef0f833505b6cd6304afb70e5
jaesukpark77 <jaesukpark77@naver.com>
2022-12-28 11:12:36 +0900

    Add: variable 학습

---
+++ client/chapter/core/02.variable.js
@@ -0,0 +1,43 @@
+/* --------------------------------------------------------------------- */
+/* Variables                                                             */
+/* --------------------------------------------------------------------- */
+
+/* eslint no-unused-vars:0 */
+let admin;
+let name = "John";
+
+admin = name;
+
+console.log(admin);
+console.log(name);
+
+let ourPlanetName = 'Earth';
+let currentUserName = 'YooJaeSeok';
+
+
+/* 다음 내용을 분석한 후, 프로그래밍 할 수 있도록 변수와 상수를 작성해봅니다. ----------- */
+
+// - 갯수 별 상품 가격 계산하기
+let calcProductQuantity;
+// - 구매 제품 가격의 총 합
+let totalProductPrice;
+// - 1년 기준 일(day)자 수
+const DAYS_PER_YEAR = 365;
+// - 구매 결제 여부
+let isPayment = false;
+let hasPaymey = true;
+// - 구매 결제 내역
+let paymentHistory;
+// - 브랜드 접두사
+const BRAND_PREFIX = "SAMSUNG";
+// - 오늘의 운세
+let todayHoroscope;
+// - 상품 정보
+const productInfo = '';
+
+
+/* variables ----------------------------------------------------------- */
+
+
+/* constant variables -------------------------------------------------- */
+
