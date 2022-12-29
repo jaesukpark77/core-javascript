93f98c9447ef062ae48e733b31f71e341c74ff30
jaesukpark77 <jaesukpark77@naver.com>
2022-12-29 09:10:57 +0900

    [Add] Type Conversion 학습 내용 추가

--- client/chapter/core/06.typeConversion.js
+++ client/chapter/core/06.typeConversion.js
@@ -1,37 +1,74 @@
+
+const user = {
+  sum : function (){
+      return this
+    }
+}
+
+user.sum()
+
+
+
 /* ---------------------------------------------------------------------- */
 /* Type Conversion                                                        */
 /* ---------------------------------------------------------------------- */
 
 
 /* 데이터 → 문자 ----------------------------------------------------------- */
-
-// number -> '2022'
+//  String()  /  'string' + ''
+// number → '2022'
 const YEAR = 2022;
 
-console.log(typeof String(YEAR));
+console.log(String(YEAR));
 
-// undefined -> 'undefined'
-// null -> 'null'
-console.log(typeof String(undefined));
+// undefined → 'undefined'
+// null → 'null'
 
-// boolean
+let days = null;
+console.log(String(days));
+
+
+// boolean → 'true' or 'false'
 let isKind = true;
-console.log(typeof String(isKind));
+console.log(isKind + '');
+
 
 /* 데이터 → 숫자 ----------------------------------------------------------- */
 
-// undeinfed
+//  Number()
 
-// null
+// undefined → NaN
+console.log(Number(undefined));
 
-// boolean
+// null → 0
+let money = null;
+console.log(Number(money));
 
-// string
+// boolean  → true : 1 / false : 0
+// *1
+// /1
+// + 
+let cutie = false;
+console.log(+cutie);
+
+
+// string 숫자형 문자 
+
+let num = '     123      '
+console.log(num * 1);
+
 
 // numeric string
+let width = '32.23120px';
+
+console.log(parseFloat(width,10));
+
 
 
 /* 데이터 → 불리언 ---------------------------------------------------------- */
 
-// null, undefined, 0, NaN, ''
+// null, undefined, 0, NaN, '' → false
 // 위에 나열한 것 이외의 것들 
+
+console.log(Boolean(123));
+console.log(!!null);
