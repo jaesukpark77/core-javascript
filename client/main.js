b867674e9fd55e0b76fcdadabd9d2107bcce88ff
jaesukpark77 <jaesukpark77@naver.com>
2023-01-13 13:16:11 +0900

    [Feat] lib 추가 및 모듈화

--- client/main.js
+++ client/main.js
@@ -1,46 +1,95 @@
+
+
+
 /* eslint no-undef:'warn' */
-/* eslint no-unused-vars:'off' */
+/* eslint no-unused-vars: 'off' */
+
+
+
+
+
+
+
+// 1. 인풋 벨류값 가져오기
+// 2. 이벤트 핸들러 연결하기
+// 3. 이벤트 기본동작 차단하기
+// 4. 두 수의 합을 더해주기 
+// 5. 화면에 출력하기
+
+
+import { 
+  getNode,
+  sum,
+  getInputValue,
+  clearContents,
+  insertLast,
+ } from "./lib/index.js";
+
+
+
+ 
+
+
 
 const firstInput = getNode('#firstNumber');
 const secondInput = getNode('#secondNumber');
 const done = getNode('#done');
+const result = getNode('.result');
 
 
-function getInputValue(node) {
-  if (typeof node === 'string') node = geteNode(node);
-  if(node.tagName !== 'INPUT') refError('getInputValue 함수는 INPUT ELEMENT만 허용합니다.')
-  return node.value
-}
 
-const sum = (valueA, valueB) => valueA + valueB;
 
-function clearContents(node) {
-  if (typeof node === 'string') node = getNode(node);
-  node.textContent = '';
-}
-
-function handler(e) {
+function handler(e){
   e.preventDefault();
 
-  // input value는 string 형태로 넘어온다 => 그래서 int 형태로 변경!!!
   let firstValue = +getInputValue(firstInput);
   let secondValue = +getInputValue(secondInput);
-  let total = sum(firstValue, secondValue);
+  let total = sum(firstValue, secondValue)
 
-  clearContents('.result');
-  insertLast('.result', total);
+
+  
+  clearContents(result);
+
+  insertLast(result,total);
+  
 }
 
-function inputHandler(e) {
+
+
+function inputHandler(){
   let firstValue = +getInputValue(firstInput);
   let secondValue = +getInputValue(secondInput);
-  let total = sum(firstValue, secondValue);
+  let total = sum(firstValue, secondValue)
 
-  clearContents('.result');
-  insertLast('.result', total);
+
+  
+  clearContents(result);
+
+  insertLast(result,total);
 }
 
-done.addEventListener('click', handler);
 
-firstInput.addEventListener('change', inputHandler)
-secondInput.addEventListener('change', inputHandler)
+
+done.addEventListener('click',handler)
+
+firstInput.addEventListener('change',inputHandler)
+secondInput.addEventListener('change',inputHandler)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
