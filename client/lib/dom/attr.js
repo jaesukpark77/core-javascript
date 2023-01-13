1fbfc432751c7441372952c64ab49cc83a033bf6
jaesukpark77 <jaesukpark77@naver.com>
2023-01-13 09:17:40 +0900

    [Chore] attr.js 수정

--- client/lib/dom/attr.js
+++ client/lib/dom/attr.js
@@ -14,14 +14,15 @@ function setAttr(node,prop,value){
   // validation : 확인 
   if(typeof node === 'string') node = getNode(node);
   if(typeof prop !== 'string') throw new TypeError('setAttr 함수의 두 번째 인자는 문자 타입 이어야 합니다.')
+  
+  if(!value) throw new SyntaxError('setAttr 함수의 세 번째 인자는 필수값입니다.');
 
   if(prop.includes('data')){
     let rest = prop.slice(5);
     node.dataset[rest] = value;
+    return;
   }
 
-  if(!value) throw new SyntaxError('setAttr 함수의 세 번째 인자는 필수값입니다.')
-  
   node.setAttribute(prop,value);
 
 }
