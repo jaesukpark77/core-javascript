c0fb07a4850fe41de52df6613ffef4334287e9d7
jaesukpark77 <jaesukpark77@naver.com>
2023-01-13 16:21:12 +0900

    [Feat] 기능 추가

--- client/main.js
+++ client/main.js
@@ -1,1 +1,31 @@
-console.log('window');
+import { clearContents, getInputValue, getNode, getRandom, insertLast, isNumericString, showAlert } from "./lib/index.js";
+import { jujeobData } from "./data/data.js";
+import { refError } from './lib/error/index.js';
+
+
+const submit = getNode('#submit');
+const resultArea = getNode('.result');
+
+function clickSubmitHandler(e) {
+  e.preventDefault();
+
+  let name = getInputValue('#nameField');
+  let list = jujeobData(name);
+  let pick = list[getRandom(list.length-1)];
+
+  if (!name) {
+    console.log('이름을 입력해 주세요!');
+    showAlert('.alert-error','잘못된 정보입니다.!',2000);
+    return;
+  }
+
+  if (isNumericString(name)) {
+    console.log('제대로된 이름을 입력해주세요.');
+    return
+  }
+
+  clearContents(resultArea);
+  insertLast(resultArea, pick)
+}
+
+submit.addEventListener('click', clickSubmitHandler)
