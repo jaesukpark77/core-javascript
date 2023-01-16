529df75ac9db304acb9bf81cf5fa46ef1c9b4509
jaesukpark77 <jaesukpark77@naver.com>
2023-01-16 11:47:51 +0900

    [Add] 레코드 리스트 및 초기화 버튼 활성화 및 비활성화

--- client/main.js
+++ client/main.js
@@ -1,4 +1,4 @@
-import { diceAnimation, getNode } from "./lib/index.js";
+import { diceAnimation, disableElement, enableElement, getNode, getNodes } from "./lib/index.js";
 
 /* 
   [주사위 굴리기]
@@ -8,9 +8,21 @@ import { diceAnimation, getNode } from "./lib/index.js";
   4. 변수 보호를 위한 클로저 + IIFE 사용하기
 */
 
+/* 
+  [레코트 리스트 보이기]
+  1. handleRecord 함수를 만들기
+  2. disable 활성 유틸 함수 만들기
+  3. handleReset 함수 만들기
+  4. visible 활성 유틸 함수 만들기
+  5. toggleState 유틸 함수 만들기
+*/
 
+// 배열의 구조분해 할당
+const [rollingDiceButton, recordButton, resetButton] = getNodes('.buttonGroup > button');
 
-const rollingDiceButton = getNode('.buttonGroup > button:nth-child(1)');
+/* const rollingDiceButton = getNode('.buttonGroup > button:nth-child(1)');
+const recordButton = getNode('.buttonGroup > button:nth-child(2)');
+const resetButton = getNode('.buttonGroup > button:nth-child(3)'); */
 
 // IIFE 패턴
 const handlerRollingDice = (() =>{
@@ -20,8 +32,12 @@ const handlerRollingDice = (() =>{
   return () => {
     if (!isRolling) {
       stopAnimation = setInterval(diceAnimation,100);
+      disableElement(recordButton);
+      disableElement(resetButton);
     }else{
       clearInterval(stopAnimation);
+      enableElement(recordButton);
+      enableElement(resetButton);
     }
   
     isRolling = !isRolling;
@@ -37,12 +53,14 @@ rollingDiceButton.addEventListener('click', handlerRollingDice);
   return () => {
     if (!isRolling) {
       stopAnimation = setInterval(diceAnimation,100);
+      disableElement(recordButton);
     }else{
       clearInterval(stopAnimation);
+      enableElement(recordButton);
     }
   
     isRolling = !isRolling;
   }
-} */
-// rollingDiceButton.addEventListener('click', handlerRollingDice())
+}
+rollingDiceButton.addEventListener('click', handlerRollingDice()) */
 // -> 이미 한 번 실행한다!!!
