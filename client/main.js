b05f7d45401c935e96e7e625cc3fb3778393d28d
jaesukpark77 <jaesukpark77@naver.com>
2023-01-16 11:21:06 +0900

    [Add] 주사위 굴리기 추가

--- client/main.js
+++ client/main.js
@@ -1,1 +1,48 @@
-console.log('window');
+import { diceAnimation, getNode } from "./lib/index.js";
+
+/* 
+  [주사위 굴리기]
+  1. dice 애니매이션 불러오기
+  2. bindEvent 유틸 함수 만들기
+  3. handleRollingDice 함수 만들고 토글로 애니메이션 제어하기
+  4. 변수 보호를 위한 클로저 + IIFE 사용하기
+*/
+
+
+
+const rollingDiceButton = getNode('.buttonGroup > button:nth-child(1)');
+
+// IIFE 패턴
+const handlerRollingDice = (() =>{
+  let isRolling = false;
+  let stopAnimation;
+  
+  return () => {
+    if (!isRolling) {
+      stopAnimation = setInterval(diceAnimation,100);
+    }else{
+      clearInterval(stopAnimation);
+    }
+  
+    isRolling = !isRolling;
+  }
+})();
+rollingDiceButton.addEventListener('click', handlerRollingDice);
+
+
+/* const handlerRollingDice = () =>{
+  let isRolling = false;
+  let stopAnimation;
+  
+  return () => {
+    if (!isRolling) {
+      stopAnimation = setInterval(diceAnimation,100);
+    }else{
+      clearInterval(stopAnimation);
+    }
+  
+    isRolling = !isRolling;
+  }
+} */
+// rollingDiceButton.addEventListener('click', handlerRollingDice())
+// -> 이미 한 번 실행한다!!!
