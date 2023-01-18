ed0dd794694b532496a2d6f14c42e2bbaf9167f5
jaesukpark77 <jaesukpark77@naver.com>
2023-01-18 11:10:39 +0900

    [Add] delayP() - Promise 활용

--- client/lib/utils/delay.js
+++ client/lib/utils/delay.js
@@ -1,10 +1,10 @@
 import { getNode } from "../dom/getNode.js";
+import { isNumber, isObject } from "./typeOf.js";
 
 const first = getNode('.first');
+const second = getNode('.second');
 
-console.log(first);
-
-function delay(callback, timeout = 1000) {
+/* function delay(callback, timeout = 1000) {
   setTimeout(callback, timeout);
 }
 
@@ -16,4 +16,75 @@ delay(()=>{
       first.style.top = '0px';
     })
   })
+}) */
+
+/* delayP()
+.then(()=>{
+  first.style.top = '-100px';
+  return delayP()
 })
+.then(()=>{
+  first.style.transform = 'rotate(360deg)';
+  second.style.left = '100px'
+  return delayP()
+})
+.then(()=>{
+  first.style.top = '0px';
+  second.style.left = '0px'
+}) */
+
+const defaultOptions = {
+  shouldReject: false,
+  timeout : 1000,
+  data : '성공',
+  errorMessage : '알 수 없는 오류가 발생했습니다.'
+}
+
+function delayP(options = {}) {
+
+  let config = {...defaultOptions} // 얕은 복사 진행
+  // let config = Object.assign({}, defaultOptions) // 얕은 복사 진행
+
+  if(isNumber(options)){
+    config.timeout = options;
+  }
+
+  // 객체 합성 mixin
+  if(isObject(options)){
+    config = {...config, ...options};
+  }
+
+  const {shouldReject, data, errorMessage, timeout} = config;
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      !shouldReject ? resolve(data) : reject(errorMessage);
+      // if(!shouldReject){
+      //   resolve('성공');
+      // }else{
+      //   reject('실패!');
+
+      // }
+    }, timeout);
+  })
+}
+
+/* delayP(3000).then((res) => {
+  console.log(res);
+}) */
+
+/* function delayP(timeout = 1000) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      // resolve('성공');
+      reject('실패!');
+    }, timeout);
+  })
+}
+delayP()
+.then((res) => {
+  console.log(res);
+})
+.catch((err) => {
+  console.log(err);
+}) */
