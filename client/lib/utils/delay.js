55eb61fcc7150806cb776818161f9e1fa601a1bd
jaesukpark77 <jaesukpark77@naver.com>
2023-01-18 11:11:44 +0900

    [Add] delayP() - Promise 활용

--- client/lib/utils/delay.js
+++ client/lib/utils/delay.js
@@ -40,7 +40,7 @@ const defaultOptions = {
   errorMessage : '알 수 없는 오류가 발생했습니다.'
 }
 
-function delayP(options = {}) {
+export function delayP(options = {}) {
 
   let config = {...defaultOptions} // 얕은 복사 진행
   // let config = Object.assign({}, defaultOptions) // 얕은 복사 진행
