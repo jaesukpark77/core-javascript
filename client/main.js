53cf5a4e6dbaf77e2098d22bb5abddddc007687b
jaesukpark77 <jaesukpark77@naver.com>
2023-01-19 13:02:40 +0900

    [Add] User List 실습

--- client/main.js
+++ client/main.js
@@ -1,5 +1,7 @@
-import { xhrData,insertLast, xhrPromise, jason } from "./lib/index.js";
-import { delayP } from './lib/utils/delay.js';
+/* global gsap */
+
+
+import { xhrData,insertLast, xhrPromise, delayP, jason, renderUserCard, getNode, changeColor, renderSpinner } from "./lib/index.js";
 
 /* xhrData.get(
   'https://jsonplaceholder.typicode.com/users',
@@ -20,11 +22,53 @@ import { delayP } from './lib/utils/delay.js';
   console.log(err);
 }) */
 
-async function render(){
+/* async function render(){
 
   await delayP(2000);
   let response = await jason.get('https://jsonplaceholder.typicode.com/users/1');
   console.log(response.data);
 }
 
-render()
+render() */
+
+// rendingUserList 함수 만들기
+// ajax(jason) get user list
+
+// 유저 카드 생성
+// 생성된 카드로 랜더링
+
+
+const userCardContainer = getNode('.user-card-inner');
+
+async function rendingUserList() {
+  
+  renderSpinner(userCardContainer);
+
+  try{
+    await delayP(2000);
+    getNode('.loadingSpinner').remove();
+  
+    let response = await jason.get('https://jsonplaceholder.typicode.com/users');
+    let userData = response.data;
+    userData.forEach(data => renderUserCard(userCardContainer, data))
+  
+  
+    console.log(gsap.utils.toArray('.user-card'));
+  
+    changeColor('.user-card')
+  
+    gsap.to(gsap.utils.toArray('.user-card'),{
+      x:0,
+      opacity:1,
+      duration:1.5,
+      stagger:0.2
+    })
+
+  }catch(err){
+    console.log(err);
+  }
+}
+
+rendingUserList()
+
+
